39af5797c1e8ef0c49bd42b5494b58dcb80921bf
candice.fays@ymail.com <candice.fays@ymail.com>
2023-08-26 21:48:18 +0200

    first commit try

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// JS pour le chargement des partiels de la section asked-section
+// TODO: add event listener on click on the alveolus to load the asked section
+
+{/* <html>
+<head>
+  <title>Mon site avec JavaScript</title>
+</head>
+<body>
+
+<div id="header"></div>
+
+<h1>Contenu principal de la page</h1>
+
+<div id="footer"></div>
+
+<script>
+  // Chargement du contenu des partiels
+  const headerDiv = document.getElementById('header');
+  const footerDiv = document.getElementById('footer');
+
+  fetch('/partials/header.html')
+    .then(response => response.text())
+    .then(content => headerDiv.innerHTML = content);
+
+  fetch('/partials/footer.html')
+    .then(response => response.text())
+    .then(content => footerDiv.innerHTML = content);
+</script>
+
+</body>
+</html> */}
